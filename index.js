8b12d02cce77cb6626ff5a09c14290e6be7f8023
samratchakraborty422000 <121852717+samratchakraborty422000@users.noreply.github.com>
2024-07-27 18:46:38 +0530

    changes done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const CryptoJS = require("crypto-js");
+require("dotenv").config()
+
 const app = express();
 
 app.use(express.json());
@@ -12,10 +14,9 @@ app.post("/", (req, res) => {
   if(!url.startsWith("https://") && !url.startsWith("http://")){
     return res.status(400).send("Invalid URL format");
   }
-  const ciphertext = CryptoJS.AES.encrypt(url, "secret key 123").toString();
+  const ciphertext = CryptoJS.AES.encrypt(url, process.env.SECRET_KEY).toString();
 
   const data = ciphertext.replace(/\+/g,'p1L2u3S').replace(/\//g,'s1L2a3S4h').replace(/=/g,'e1Q2u3A4l');
-  console.log(data);
   res.status(201).send(data);
 });
 
@@ -26,12 +27,9 @@ app.get("/:hash", (req, res) => {
   }
 
   const newHashUrl = hash.replace(/p1L2u3S/g, '+' ).replace(/s1L2a3S4h/g, '/').replace(/e1Q2u3A4l/g, '=');;
-  var bytes = CryptoJS.AES.decrypt(newHashUrl, "secret key 123");
+  var bytes = CryptoJS.AES.decrypt(newHashUrl, process.env.SECRET_KEY);
   var originalText = bytes.toString(CryptoJS.enc.Utf8);
 
-
-
-  console.log(originalText); // 'my message
   res.status(301).redirect(originalText)
   
 });
